a0d6e882f128f2fe2df590a13c927d1c9761480e
kimberli <kimberlizhong@gmail.com>
2017-12-06 20:48:35 -0500

    add hammerspoon

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Finally version controlling these.
 Set up by cloning this repo then running `setup.sh` (make sure to do this from within this dotfiles directory).
 
 ![screenshot](https://raw.githubusercontent.com/kimberli/dotfiles/master/ex.png)
+
+### Hammerspoon
+I've also included my [Hammerspoon](http://www.hammerspoon.org/) `init.lua` script. It's pretty janky and includes:
+
+* window resizing
+* jumping to popular apps
+* an ugly Slack API polling menubar item
+
+Symlink it similarly to use it.
---
+++ hammerspoon/init.lua
@@ -0,0 +1,127 @@
+--
+-- window resizing
+--
+function baseMove(x, y, w, h)
+    return function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.w * x + max.x
+        f.y = max.h * y + max.y
+        f.w = max.w * w
+        f.h = max.h * h
+        win:setFrame(f, 0)
+    end
+end
+
+hs.hotkey.bind({'ctrl', 'cmd'}, 'H', baseMove(0, 0, 0.5, 1))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'L', baseMove(0.5, 0, 0.5, 1))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'J', baseMove(0, 0.5, 1, 0.5))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'K', baseMove(0, 0, 1, 0.5))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'M', baseMove(0, 0, 1, 1))
+
+--
+-- jump to applications
+--
+hs.hotkey.bind({'ctrl', 'cmd'}, 'S', function () hs.application.launchOrFocus("Slack") end)
+hs.hotkey.bind({'ctrl', 'cmd'}, 'G', function () hs.application.launchOrFocus("Google Chrome") end)
+hs.hotkey.bind({'ctrl', 'cmd'}, 'I', function () hs.application.launchOrFocus("iTerm") end)
+
+--
+-- slack menubar item
+--
+slack = hs.menubar.new()
+paused = false
+logger = hs.logger.new("slack", "debug")
+interval = 15  -- seconds in between polls
+lastRunTime = ""
+
+channelListUrl = "https://slack.com/api/channels.list"
+channelInfoUrl = "https://slack.com/api/channels.info"
+
+token = "xoxp-your-token-here"
+
+function watchSlack()
+    getUnreadMsgs()
+end
+
+function updateMenu()
+    slack:setMenu({
+        { title = paused and "Resume" or "Pause", fn = togglePause },
+        { title = msg, disabled = true },
+        { title = "Last run at " .. lastRunTime, disabled = true }
+    })
+end
+
+function togglePause()
+    if paused then
+        paused = false
+        watchSlack()
+    else
+        paused = true
+        updateMenu()
+    end
+end
+
+function setSlackIcon(active, msg)
+    lastRunTime = os.date("%I:%M:%S %p")
+    updateMenu()
+    if active then
+        slack:setIcon(hs.image.imageFromPath("~/.hammerspoon/icons/slack_active.png"), false)
+    else
+        slack:setIcon(hs.image.imageFromPath("~/.hammerspoon/icons/slack_default.png"), false)
+    end
+    if not paused then
+        hs.timer.doAfter(interval, getUnreadMsgs)
+    end
+end
+
+-- this is ugly but i don't know better lua
+function getUnreadMsgs()
+    channel_x = 1
+    function poll_next_channel()
+        id = channels[channel_x]["id"]
+        logger.i("id: " .. id)
+        url = channelInfoUrl .. "?channel=" .. id
+        logger.i("checking " .. url)
+        hs.http.asyncGet(url, {["Authorization"] = "Bearer " .. token }, check_channel)
+    end
+    function check_channel(resp_code, resp_body, _)
+        if resp_code ~= 200 then
+            setSlackIcon(false, "Failed - " .. resp_code)
+            return
+        end
+        unreads = hs.json.decode(resp_body)["channel"]["unread_count"]
+        if unreads ~= nil and unreads > 0 then
+            setSlackIcon(true, unreads .. " unread message(s)")
+            return
+        else
+            channel_x = channel_x + 1
+            if channel_x <= #channels then
+                poll_next_channel()
+            else
+                setSlackIcon(false, "No new messages")
+            end
+        end
+    end
+    function poll_channels(resp_code, resp_body, _)
+        if resp_code ~= 200 then
+            setSlackIcon(false, "Failed - " .. resp_code)
+            return
+        end
+        channels = hs.json.decode(resp_body)["channels"]
+        if channels ~= nil then
+            poll_next_channel()
+        else
+            setSlackIcon(false, "Failed - invalid channel list")
+        end
+    end
+    hs.http.asyncGet(channelListUrl, {["Authorization"] = "Bearer " .. token }, poll_channels)
+end
+
+-- start watching slack
+if slack then
+    watchSlack()
+end
